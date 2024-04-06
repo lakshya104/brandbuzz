cbb1123054dc07b26e4d4e1c80adf968a5868808
lakshya104 <lakshya.vyas@anktech.co.in>
2024-04-06 15:10:31 +0530

    logo added

--- app/(landing)/header.jsx
+++ app/(landing)/header.jsx
@@ -9,13 +9,14 @@ import { Loader } from "lucide-react";
 //   UserButton,
 // } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import logo from '../../public/next.svg'
 
 export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <Image src={logo} height={40} width={40} alt="Mascot" />
           <h1 className="text-2xl font-extrabold text-sky-600 tracking-wide">
            Buzz
           </h1>
--- app/(landing)/page.jsx
+++ app/(landing)/page.jsx
@@ -1,27 +1,3 @@
-// import { Button } from "@/components/ui/button";
-
-// export default function Home() {
-//   return (
-//     <div className="flex flex-col justify-center items-center h-full">
-//       <Button variant="sidebar">sidebar</Button>
-//       <Button variant="secondary">secondary</Button>
-//       {/* <Button variant="locked">locked </Button>
-//       <Button variant="default"> default</Button>
-//       <Button variant="primary">primary </Button>
-//       <Button variant="primaryOutline"> primaryOutline</Button>
-//       <Button variant="secondaryOutline"> secondaryOutline</Button>
-//       <Button variant="danger"> danger</Button>
-//       <Button variant="dangerOutline">dangerOutline </Button>
-//       <Button variant="super">super </Button>
-//       <Button variant="superOutline"> superOutline</Button> */}
-//       <Button variant="ghost"> ghost</Button>
-//       <Button variant="sidebarOutline"> sidebarOutline</Button>
-
-//     </div>
-//   );
-// }
-
-
 import Image from "next/image";
 import { Loader } from "lucide-react";
 // import { 
@@ -34,12 +10,13 @@ import { Loader } from "lucide-react";
 // } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import hero from '../../public/next.svg'
 
 export default function Home() {
   return (
     <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
       <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
-        <Image src="/hero.svg" fill alt="Hero" />
+        <Image src={hero} fill alt="Hero" />
       </div>
       <div className="flex flex-col items-center gap-y-8">
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
