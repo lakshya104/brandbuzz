56f5a2c4f31f3aa2457bc8b511863e348cdbc42c
lakshya104 <lakshya.vyas@anktech.co.in>
2024-04-11 18:20:24 +0530

    secret updated

--- auth.js
+++ auth.js
@@ -11,5 +11,6 @@ export const {
 } = NextAuth({
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
+    secret: '12AfIfb9ViAVr20IRPezPdunXYPbpBDHZ+veaRXQ15A=',
   ...authConfig,
 });
