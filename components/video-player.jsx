03f25ba60ab83e52d2c9bb9b7b0557fd8538ead4
lakshya104 <lakshya.vyas@anktech.co.in>
2024-04-16 18:54:41 +0530

    minor ui change for mobile view

--- components/video-player.jsx
+++ components/video-player.jsx
@@ -7,8 +7,8 @@ const VideoPlayer = ({ title, videosrc, description }) => {
     <>
       <h1 className="text-xl my-2 text-sky-900 font-medium">{title}</h1>
         <ReactPlayer
-          width="370px"
-          height="215px"
+          width="360px"
+          height="212px"
           url={videosrc}
           playing={true}
           controls={true}
@@ -16,7 +16,7 @@ const VideoPlayer = ({ title, videosrc, description }) => {
           pip={true}
           muted={true}
           stopOnUnmount={false}
-          className="bg-slate-950 p-2 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] "
+          className="bg-slate-950 p-0.5 lg:p-2 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] "
         />
       <source src={videosrc} type="video/mp4" />
       <p className="lg:px-12 lg:py-4 p-2 my-2 text-sm text-center">{description}</p>
