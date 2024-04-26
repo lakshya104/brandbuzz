dea956b906fe47892f7f0bdb43f168b8fdbf0541
lakshya104 <lakshya.vyas@anktech.co.in>
2024-04-26 12:20:04 +0530

    leaderboard made client component

--- app/(main)/leaderboard/points-table.jsx
+++ app/(main)/leaderboard/points-table.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { getLeaderboard } from "@/actions/redeem";
 import {
   Table,
   TableBody,
@@ -8,40 +10,52 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-
+import { useEffect, useState } from "react";
 
 const PointsTable = ({pointBoard}) => {
+  const [leaderBoard, setLeaderboard] = useState(pointBoard);
+
+  useEffect(() => {
+    const fetchLeaderboard = async () => {
+      try {
+        const pointBoard = await getLeaderboard();
+        setLeaderboard(pointBoard);
+      } catch (error) {
+        console.error("Error fetching leaderboard:", error);
+      }
+    };
+
+    fetchLeaderboard();
+  }, []);
+
   return (
-    // <Table>
-    //   <TableCaption>This is a Leaderboard.</TableCaption>
-    //   <TableHeader>
-    //     <TableRow>
-    //       <TableHead className="w-[100px]">User</TableHead>
-    //       <TableHead>Email</TableHead>
-    //       <TableHead className="text-right">Points</TableHead>
-    //     </TableRow>
-    //   </TableHeader>
-    //   <TableBody>
-    //     {pointBoard.map((item) => (
-    //       <TableRow key={item.id}>
-    //         <TableCell className="font-medium">{item.name}</TableCell>
-    //         <TableCell>{item.email}</TableCell>
-    //         <TableCell className="text-right font-bold">{item.points}</TableCell>
-    //       </TableRow>
-    //     ))}
-    //   </TableBody>
-    //   <TableFooter>
-    //     {/* <TableRow>
-    //       <TableCell colSpan={3}>Total</TableCell>
-    //       <TableCell className="text-right">$2,500.00</TableCell>
-    //     </TableRow> */}
-    //   </TableFooter>
-    // </Table>
-    <>
-    {pointBoard.map((item)=>(
-        <p key={item.id}>{item.points}</p>
-    ))}
-    </>
+    <Table>
+      <TableCaption>This is a Leaderboard.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">User</TableHead>
+          <TableHead>Email</TableHead>
+          <TableHead className="text-right">Points</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {leaderBoard.map((item) => (
+          <TableRow key={item.id}>
+            <TableCell className="font-medium">{item.name}</TableCell>
+            <TableCell>{item.email}</TableCell>
+            <TableCell className="text-right font-bold">
+              {item.points}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        {/* <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className="text-right">$2,500.00</TableCell>
+        </TableRow> */}
+      </TableFooter>
+    </Table>
   );
 };
 
