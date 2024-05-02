81eda92fcd9900d99663875e713309cf69244344
lakshya104 <lakshya.vyas@anktech.co.in>
2024-05-02 12:34:50 +0530

    added loading skeleton

--- app/(nested)/quiz/[id]/questions.jsx
+++ app/(nested)/quiz/[id]/questions.jsx
@@ -3,12 +3,13 @@ import { createUserAnswer, getAnswersByQuestionId } from "@/actions/redeem";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { Ban, Check } from "lucide-react";
-import React, { useEffect, useState } from "react";
+import { Ban, Check, LoaderCircle } from "lucide-react";
+import React, { useEffect, useState, useTransition } from "react";
 
 const Questions = ({ ques, id, inc, dec }) => {
   const [answers, setAnswers] = useState({});
   const [loadingBtn, setLoadingBtn] = useState(true);
+  const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
     const fetchAnswers = async () => {
@@ -25,7 +26,9 @@ const Questions = ({ ques, id, inc, dec }) => {
       }
     };
 
+    // startTransition(() => {
     fetchAnswers();
+    // });
   }, [ques]);
 
   const handleClick = (id, answer) => {
@@ -45,53 +48,60 @@ const Questions = ({ ques, id, inc, dec }) => {
             {question.text}
           </h2>
           <ul className="mb-5">
-            {answers[question.id] && (
+            {answers[question.id] ? (
               <>
                 {answers[question.id].map((answer) => (
                   <li key={answer.id} className="ml-5">
                     <div className="flex justify-start items-center">
-                  <Dialog>
-                    <DialogTrigger asChild>
-                      {loadingBtn ? (
-                        <Skeleton className="h-[38px] bg-sky-100 rounded-lg my-2 w-[300px]" />
-                      ) : (
-                        <Button
-                          // disabled={isPending || answeredQuestions[index]}
-                          onClick={()=>handleClick(id, answer)
-                        }
-                          className="my-1 text-[12px] lg:text-[15px]"
-                        >
-                          {answer.text}
-                          {/* {isPending && (
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          {isPending ? (
+                            <Skeleton className="h-[38px] bg-sky-100 rounded-lg my-2 w-[300px]" />
+                          ) : (
+                            <Button
+                              disabled={isPending}
+                              onClick={() => handleClick(id, answer)}
+                              className="my-1 text-[12px] lg:text-[15px]"
+                            >
+                              {answer.text}
+                              {/* {isPending && (
                             <LoaderCircle className="animate-spin ml-2 h-5 w-5 text-slate-600" />
                           )} */}
-                        </Button>
-                      )}
-                    </DialogTrigger>
-                    <DialogContent className="shadow-inner w-[300px] py-8 rounded- px-4 lg:px-12 z-50">
-                      <div className="flex items-center justify-center flex-col space-y-4">
-                        {answer.isCorrect ? (
-                          <div className="flex items-center justify-center">
-                            <Check className="text-green-600 stroke-[4] mr-2 h-6 w-6" />
-                            <h3 className="font-semibold text-green-600">
-                              You got it right!
-                            </h3>
+                            </Button>
+                          )}
+                        </DialogTrigger>
+                        <DialogContent className="shadow-inner w-[300px] py-8 rounded- px-4 lg:px-12 z-50">
+                          <div className="flex items-center justify-center flex-col space-y-4">
+                            {answer.isCorrect ? (
+                              <div className="flex items-center justify-center">
+                                <Check className="text-green-600 stroke-[4] mr-2 h-6 w-6" />
+                                <h3 className="font-semibold text-green-600">
+                                  You got it right!
+                                </h3>
+                              </div>
+                            ) : (
+                              <div className="flex items-center justify-center">
+                                <Ban className="text-red-600 mr-2 stroke-[4] h-4 w-4" />
+                                <h3 className="font-semibold text-red-600">
+                                  Oops! Not correct.
+                                </h3>
+                              </div>
+                            )}
                           </div>
-                        ) : (
-                          <div className="flex items-center justify-center">
-                            <Ban className="text-red-600 mr-2 stroke-[4] h-4 w-4" />
-                            <h3 className="font-semibold text-red-600">
-                              Oops! Not correct.
-                            </h3>
-                          </div>
-                        )}
-                      </div>
-                    </DialogContent>
-                  </Dialog>
-                </div>
+                        </DialogContent>
+                      </Dialog>
+                    </div>
                   </li>
                 ))}
               </>
+            ) : (
+              <>
+              <Skeleton className="h-[38px] bg-sky-100 rounded-lg my-2 w-[300px]" />
+              <Skeleton className="h-[38px] bg-sky-100 rounded-lg my-2 w-[300px]" />
+              <Skeleton className="h-[38px] bg-sky-100 rounded-lg my-2 w-[300px]" />
+              <Skeleton className="h-[38px] bg-sky-100 rounded-lg my-2 w-[300px]" />
+              </>
+
             )}
           </ul>
         </div>
