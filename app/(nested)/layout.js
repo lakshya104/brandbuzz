e50087e85e2258a3b71259b63303a12f53107c05
lakshya104 <lakshya.vyas@anktech.co.in>
2024-05-02 16:31:36 +0530

    not found added

--- app/(nested)/layout.js
+++ app/(nested)/layout.js
@@ -9,7 +9,7 @@ const LandingLayout = ({ children }) => {
       <main className="flex-1 flex flex-col items-center justify-center">
         {children}
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
